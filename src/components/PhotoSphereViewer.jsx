a3aa85585bedd68243b28235d768c8468d2cd557
Putra Sedana <putrasedana03@gmail.com>
2024-10-28 14:28:02 +0800

    commit 4

--- src/components/PhotoSphereViewer.jsx
+++ src/components/PhotoSphereViewer.jsx
@@ -15,52 +15,57 @@ const PhotoSphereViewer = () => {
   const markersPluginRef = useRef(null);
 
   const [areAllTooltipsVisible, setAllTooltipsVisible] = useState(false);
-  const markerIcon = `
-        <div style="
-            display: flex; 
-            flex-direction: column; 
-            align-items: center; 
-            width: 60px; 
-            height: 80px; 
-            position: relative;">
-            <div style="
-                width: 40px; 
-                height: 40px; 
-                background-color: #00808F; 
-                border-radius: 30%; 
-                display: flex; 
-                justify-content: center; 
-                align-items: center; 
-                color: white;">
-                <!-- SVG icon -->
-                <svg width="23" height="25" viewBox="0 0 23 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M21.6942 22.9167H20.6612V21.875C20.6612 21.25 20.2479 20.8333 19.6281 20.8333H18.595V10.4167H20.6612L22.7273 6.25C20.6612 6.45833 18.595 6.45833 16.5289 6.25C14.876 5 13.6364 3.75 12.3967 2.08333V1.04167C12.3967 0.416667 11.9835 0 11.3636 0C10.7438 0 10.3306 0.416667 10.3306 1.04167V2.08333C9.09091 3.75 7.85124 5 6.19835 6.25C4.13223 6.45833 2.06612 6.45833 0 6.25L2.06612 10.4167H4.13223V20.8333H3.09917C2.47934 20.8333 2.06612 21.25 2.06612 21.875V22.9167H1.03306C0.413223 22.9167 0 23.3333 0 23.9583V25H22.7273V23.9583C22.7273 23.3333 22.314 22.9167 21.6942 22.9167ZM10.3306 20.8333H6.19835V10.4167H10.3306V20.8333ZM16.5289 20.8333H12.3967V10.4167H16.5289V20.8333Z" fill="white"/>
-                </svg>
-            </div>
-            <div style="
-                width: 0;
-                height: 0;
-                border-left: 5px solid transparent;
-                border-right: 5px solid transparent;
-                border-top: 5px solid #00808F;
-            "></div>
-            <div style="
-                width: 1px; 
-                height: 30px; 
-                background-color: #FFF; 
-                margin-top: 2px;">
-            </div>
-            <div style="
-                width: 8px; 
-                height: 8px; 
-                background-color: #fff ; 
-                border-radius: 50%; 
-                position: absolute; 
-                bottom: 0;"
-                >
-            </div>
-        </div>
-    `;
+
+  const markerContent = `
+  <div
+  style="
+    width: 30px;
+    height: 30px;
+    background-color: #00808f;
+    border-radius: 30%;
+    margin: auto;
+    color: white;
+  "
+>
+  <svg
+    width="23"
+    height="25"
+    viewBox="0 0 23 25"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M21.6942 22.9167H20.6612V21.875C20.6612 21.25 20.2479 20.8333 19.6281 20.8333H18.595V10.4167H20.6612L22.7273 6.25C20.6612 6.45833 18.595 6.45833 16.5289 6.25C14.876 5 13.6364 3.75 12.3967 2.08333V1.04167C12.3967 0.416667 11.9835 0 11.3636 0C10.7438 0 10.3306 0.416667 10.3306 1.04167V2.08333C9.09091 3.75 7.85124 5 6.19835 6.25C4.13223 6.45833 2.06612 6.45833 0 6.25L2.06612 10.4167H4.13223V20.8333H3.09917C2.47934 20.8333 2.06612 21.25 2.06612 21.875V22.9167H1.03306C0.413223 22.9167 0 23.3333 0 23.9583V25H22.7273V23.9583C22.7273 23.3333 22.314 22.9167 21.6942 22.9167ZM10.3306 20.8333H6.19835V10.4167H10.3306V20.8333ZM16.5289 20.8333H12.3967V10.4167H16.5289V20.8333Z"
+      fill="white"
+    />
+  </svg>
+</div>
+<div
+  style="
+    width: 0;
+    height: 0;
+    border-left: 5px solid transparent;
+    border-right: 5px solid transparent;
+    border-top: 5px solid #00808f;
+    margin: auto;
+  "
+></div>
+<div style="display: flex; flex-direction: column-reverse; align-items: center">
+  <div
+    style="width: 1px; height: 40px; background-color: #fff; margin-top: 5px"
+  ></div>
+  <div
+    style="
+      width: 8px;
+      height: 8px;
+      background-color: #fff;
+      border-radius: 50%;
+      position: absolute;
+      bottom: 0;
+    "
+  ></div>
+</div>
+  `;
 
   const customContent = `
     <div style="margin-bottom: 10px; width: 25px; height: 25px;">
@@ -138,10 +143,10 @@ const PhotoSphereViewer = () => {
             markers: [
               {
                 id: 'image',
-                position: { yaw: 0.02, pitch: -0.02 },
+                position: { yaw: 0.04, pitch: -0.08 },
                 // image: baseUrl + 'pictos/pin-blue.png',
-                html: markerIcon,
-                size: { width: 32, height: 32 },
+                html: markerContent,
+                size: { width: 80, height: 80 },
                 anchor: 'bottom center',
                 zoomLvl: 100,
                 tooltip: {
